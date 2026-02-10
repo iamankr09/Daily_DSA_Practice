7738c603962df3004785524ce6f00475b1059f91
Aman kumar <108830462+iamankr09@users.noreply.github.com>
2026-02-10 19:44:26 +0530

    [258] [Time Beats: N/A] [Memory Beats: N/A] - LeetPush

---
+++ Add-Digits.cpp
@@ -0,0 +1,22 @@
+1class Solution {
+2public:
+3    int sumOfDigits(int n){
+4        int sum = 0;
+5        while(n > 0){
+6            int lastDigit = n%10;
+7            sum += lastDigit;
+8            n = n/10;
+9        }
+10
+11        return sum;
+12    }
+13    int addDigits(int num) {
+14        int ans = 0;
+15        while(num >= 10){
+16            ans = sumOfDigits(num);
+17            num = ans;
+18        }
+19
+20        return ans;
+21    }
+22};
